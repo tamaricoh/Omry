457f5ae1ab3256bb32e123d6286fd4644c70c7f2
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2023-05-04 12:51:44 +0300

    ex1_working

--- ex3.py
+++ ex3.py
@@ -58,7 +58,6 @@ class Net(nn.Module):
 
 
 # -------------------------------------------------------------------- Define the cross entropy loss function and optimizer
-# model = Net(input_size, hidden_size, num_classes)
 model = Net(input_size, hidden_size, num_classes).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -88,22 +87,13 @@ def evaluate(model, dataloader, criterion):
     running_accuracy = 0
     with torch.no_grad():
         for data, target in dataloader:
-            # data = data.to(device)
             data = data.reshape(-1, input_size).to(device)
             target = target.to(device)
             output = model(data)
             loss = criterion(output, target)
-            # running_loss += loss.item() * data.size(0)
-            # # pred = output.argmax(dim=1, keepdim=True)
-            # predicted = torch.max(output, 1)
-            # running_accuracy += (predicted == target).sum().item()
-            # # running_accuracy += pred.eq(target.view_as(pred)).sum().item()
-            # # running_accuracy = 100. * \
-            # # running_accuracy / len(dataloader.dataset)
             running_loss += loss.item()
             _, predicted = torch.max(output.data, 1)
             running_accuracy += (predicted == target).sum().item()
-            # running_accuracy += torch.sum(predicted == target)
     epoch_loss = running_loss / len(dataloader)
     epoch_accuracy = 100. * running_accuracy / len(dataloader.dataset)
     return epoch_loss, epoch_accuracy
@@ -118,7 +108,6 @@ test_acc_history = []
 
 total_step = len(train_dataloader)
 for epoch in range(num_epochs):
-    # for i, (data, label) in enumerate(train_dataloader):
     train(model, train_dataloader, optimizer, criterion)
     train_loss, train_acc = evaluate(model, train_dataloader, criterion)
     test_loss, test_acc = evaluate(model, test_dataloader, criterion)
@@ -152,62 +141,5 @@ for i, idx in enumerate(misclassified):
     axs[i // 5, i % 5].imshow(img.squeeze().numpy(), cmap='gray')
     axs[i // 5, i % 5].set_title(f'True: {label}, Pred: {pred[idx][0]}')
     axs[i // 5, i % 5].axis('off')
-    # plt.savefig("output_nums.jpg")
-    plt.show()
+plt.show()
 # -------------------------------------------------------------------- //
-
-# # Train the network
-# last_lose_test = []
-# last_lose_train = []
-# train_loss_history = []
-# test_loss_history = []
-# test_acc_history = []
-# for epoch in range(5):
-#     train(model, train_dataloader, optimizer, criterion)
-#     train_loss, train_acc = evaluate(model, train_dataloader, criterion)
-#     test_loss, test_acc = evaluate(model, test_dataloader, criterion)
-#     train_loss_history.append(train_loss)
-#     test_loss_history.append(test_loss)
-#     test_acc_history.append(test_acc)
-
-
-# print("std of train : ", np.std(last_lose_train))
-# print("std of test : ", np.std(last_lose_test))
-# print("mean of train : ", np.mean(last_lose_train))
-# print("mean of test : ", np.mean(last_lose_test))
-# # Plot the train and test loss curves
-
-
-# plt.plot(train_loss_history, label='Train Loss')
-# plt.plot(test_loss_history, label='Test Loss')
-# # plt.legend()
-# # plt.show()
-# plt.savefig("output_plt.jpg")
-
-# # if task == 1:
-# # Plot some of the misclassified images
-# misclassified = []
-# model.eval()
-# with torch.no_grad():
-#     for data, target in test_dataloader:
-#         output = model(data)
-#         pred = output.argmax(dim=1, keepdim=True)
-#         misclassified.extend([i for i, x in enumerate(
-#             pred.eq(target.view_as(pred))) if not x])
-#         if len(misclassified) >= 20:
-#             break
-# fig, axs = plt.subplots(4, 5, figsize=(10, 8))
-# for i, idx in enumerate(misclassified):
-#     img, label = test_dataset[idx]
-#     axs[i // 5, i % 5].imshow(img.squeeze().numpy(), cmap='gray')
-#     axs[i // 5, i % 5].set_title(f'True: {label}, Pred: {pred[idx][0]}')
-#     axs[i // 5, i % 5].axis('off')
-# # plt.savefig("output_nums.jpg")
-# plt.show()
-
-
-# last_lose_train.append(train_loss_history[4])
-# last_lose_test.append(test_loss_history[4])
-
-# print('Accuracy of the network on the 10000 test images: {} %'.format(
-#     100 * correct / total))
